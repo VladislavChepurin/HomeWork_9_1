e4c21f3059c03b4d237443911ab78163ab656cb1
Vladislav <vlad1988_1@mail.ru>
2022-06-19 14:23:17 +0300

    internal void Read()

--- HomeWork_9_2/ReadConcole.cs
+++ HomeWork_9_2/ReadConcole.cs
@@ -7,7 +7,7 @@ internal class ReadConcole
     public event Notify? NotifiEvent;
     UserException userException = new UserException("Введено некоректное заначение.");// Побережем память машины не будем создавать мусор в каждой ирретации.
 
-    public void Read()
+    internal void Read()
     {
         Console.WriteLine("Введите чило 1 или 2");
         if (int.TryParse(Console.ReadLine(), out int  result) && (result == 1 || result == 2))
